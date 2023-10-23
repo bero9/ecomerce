93c970230a27448f9593e8b9823689b7433f6e80
bero9 <berohamood545@gmail.com>
2023-10-23 17:44:06 +0300

    init commit

---
+++ lib/Homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: Container(
+          padding: EdgeInsets.all(20),
+           child: ListView(children: [
+             Row(children: [
+               Expanded(child: TextFormField(
+                 decoration: InputDecoration(
+                   prefixIcon: Icon(Icons.search),
+                   hintText: "Search",
+                   border: InputBorder.none,fillColor: Colors.grey[200] ,filled: true,),
+
+               )),
+               Padding(
+                 padding:  EdgeInsets.only(left:10,right: 10),
+                 child: Icon(Icons.menu,size: 40,),
+               )
+             ],),
+             Container( height: 30,),
+
+             Text("Categories",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)  ,),
+
+
+           ],
+           ),
+      ),
+    );
+  }
+}
